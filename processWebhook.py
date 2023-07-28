beea86e24e9dbef7e0d0dec2cc1c2f6b31d9994f
kremeth <66464637+kremeth@users.noreply.github.com>
2023-07-28 12:45:37 +0800

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -1,42 +1,45 @@
 import flask
 import os
 import openai
-from flask_cors import CORS  # Import the flask-cors extension
-
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 CORS(app)
 
 @app.route('/')
-@app.route('/home', methods=['POST'])  # Only accept POST requests for this route
+@app.route('/home', methods=['POST'])
 def home():
     # Usage example
     os.environ["PYDEVD_WARN_EVALUATION_TIMEOUT"] = "10000"  # Timeout in milliseconds
-    data = flask.request.json  # Get the JSON data from the request body
-    prompt = data.get('text', '')  # Get the 'text' parameter from the JSON data
+    data = flask.request.json
 
-    callback = flask.request.args.get('callback', 'jsonpCallback')  # Get the 'callback' query parameter from the URL
+    if not data:
+        return "Invalid JSON data", 400
 
-    # Extract the API key from the query parameter 'api_key'
+    # Check if the 'text' parameter is present in the JSON data
+    if 'text' not in data:
+        return "Missing 'text' parameter in JSON data", 400
+
+    prompt = data.get('text', '')
+
+    callback = flask.request.args.get('callback', 'jsonpCallback')
     api_key = flask.request.args.get('api_key', '')
 
     if not prompt:
         return f'{callback}("Waiting");'
 
     try:
-        openai.api_key = api_key  # Use the extracted API key
+        openai.api_key = api_key
         response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1000)
         if 'choices' in response and len(response.choices) > 0:
-            response_text = response.choices[0].text.replace('"', '\\"')  # Escape double quotes in the response
-            # Clean the response text from '?' and '\n'
+            response_text = response.choices[0].text.replace('"', '\\"')
             cleaned_response_text = response_text.replace('?', '').replace('\n', '')
             return f'{callback}({{"response": "{cleaned_response_text}"}});'
         else:
-            return f'{callback}({{"response": ""}});'  # Return an empty response if there is no data
+            return f'{callback}({{"response": ""}});'
     except Exception as e:
         return f'{callback}({{"response": "Error: {str(e)}"}});'
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
-    # app.debug = True
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
