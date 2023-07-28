9059a8eb78a67be0360ec5a3d616946be37958fc
kremeth <66464637+kremeth@users.noreply.github.com>
2023-07-28 11:36:18 +0800

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -1,4 +1,3 @@
-
 import flask
 import os
 import openai
@@ -11,18 +10,21 @@ def home():
     # Usage example
     os.environ["PYDEVD_WARN_EVALUATION_TIMEOUT"] = "10000"  # Timeout in milliseconds
     prompt = flask.request.args.get('text', '')  # Get the 'text' query parameter from the URL
+    callback = flask.request.args.get('callback', 'jsonpCallback')  # Get the 'callback' query parameter from the URL
+
     if not prompt:
-        return f'{flask.request.args.get("callback", "jsonpCallback")}("Waiting");'
+        return f'{callback}("Waiting");'
 
     try:
         openai.api_key = "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"
         response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1000)
         if 'choices' in response and len(response.choices) > 0:
-            return f'{flask.request.args.get("callback", "jsonpCallback")}({{"response": "{response.choices[0].text}"}});'
+            response_text = response.choices[0].text.replace('"', '\\"')  # Escape double quotes in the response
+            return f'{callback}({{"response": "{response_text}"}});'
         else:
-            return f'{flask.request.args.get("callback", "jsonpCallback")}("Error: Empty response from OpenAI");'
+            return f'{callback}("Error: Empty response from OpenAI");'
     except Exception as e:
-        return f'{flask.request.args.get("callback", "jsonpCallback")}("Error: {str(e)}");'
+        return f'{callback}("Error: {str(e)}");'
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
