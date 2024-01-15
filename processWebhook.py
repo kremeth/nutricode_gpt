99e2fee3b9322197f9f7caed69a190361975f667
kremeth <66464637+kremeth@users.noreply.github.com>
2024-01-15 20:07:44 +0800

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -5,7 +5,6 @@ import openai
 from flask_cors import CORS
 from openai import OpenAI
 
-
 app = flask.Flask(__name__)
 CORS(app)
 
@@ -33,9 +32,9 @@ def home():
 
     try:
         openai.api_key = api_key
-        
         client = OpenAI(api_key=api_key)
-        chat_completion = client.chat.completions.create(
+
+        response = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
@@ -43,9 +42,13 @@ def home():
                 }
             ],
             model="gpt-3.5-turbo",
-        )
-        return f'{{"response": "{chat_completion.choices[0].message.content}"}}'
-
+        )        
+        if 'choices' in response and len(response.choices) > 0:
+            response_text = response.choices[0].text.replace('"', '\\"')
+            cleaned_response_text = response_text.replace('?', '').replace('\n', '')
+            return f'{{"response": "{cleaned_response_text}"}}'
+        else:
+            return '{"response": ""}'
     except Exception as e:
         return f'{{"response": "Error: {str(e)}"}}'
 
